0073bfca3024afb61734aff325d1d45cc16715e2
Luu Tuan Minh <reddevilpro9x@gmail.com>
2018-06-23 20:51:13 +0700

    Commited by Minh

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -22,22 +22,20 @@
             ->join('tbl_orders AS o', 'c.id', '=', 'o.customer_id')
             ->join('tbl_product_orders AS po', 'o.id', 'po.order_id')
             ->join('tbl_products AS p', 'po.product_id', 'p.id');
-
          $keywords = Input::get('q');
 
          if (isset($keywords)) {
             $orders = $ordersJoined
-               ->groupBy('c.id')
+               ->groupBy('o.id')
                ->select('o.id AS orderID', 'c.name AS customerName', 'email', 'phone', 'address', 'order_day', 'total')
                ->where('c.name', 'like', "%$keywords%")
                ->paginate(10);
          } else {
             $orders = $ordersJoined
-               ->groupBy('c.id')
+               ->groupBy('o.id')
                ->select('o.id AS orderID', 'c.name AS customerName', 'email', 'phone', 'address', 'order_day', 'total')
                ->paginate(10);
          }
-
          return view('admin.orders.index', ['orders' => $orders]);
       }
    }
